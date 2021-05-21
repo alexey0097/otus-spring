81c23d268ebd34c9eab764dc06cebeae7a288d31
Alexey Zemtsov <azemtsov@diasoft.ru>
2021-05-21 17:33:45 +0300

    дз по jpa crud repository

--- spring-09-homework/src/main/java/ru/diasoft/spring/homework/repository/CommentRepository.java
+++ spring-09-homework/src/main/java/ru/diasoft/spring/homework/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package ru.diasoft.spring.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.diasoft.spring.homework.entity.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> { }
